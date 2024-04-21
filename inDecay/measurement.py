a7ee2cf077c9210e953c8b8e1fdc384b563a78ca
oZwZo <zhengwzh5@hotmail.com>
2024-04-22 06:00:12 +0800

    updates

--- inDecay/measurement.py
+++ inDecay/measurement.py
@@ -8,10 +8,7 @@ import pickle as pkl
 sys.path.append(PATH.STpyutils_dir)
 # -- lindel utils --
 sys.path.append(PATH.Lindel_dir)
-from Predictor import gen_indel , onehotencoder, create_feature_array
-from selftarget.indel import tokFullIndel
 import matplotlib.pyplot as plt
-from scipy.special import softmax
 global data_dir
 global combine_train
 global combine_test
