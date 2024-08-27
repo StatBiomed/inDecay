7061714b8198e18a23c58a0d4e6ac2cd3d8eae2a
louisa9634 <1003139634@qq.com>
2024-08-27 19:44:10 +0800

    auto installing indelgen!

--- scripts/selftarget.sh
+++ scripts/selftarget.sh
@@ -1,16 +1,20 @@
 main_dir=$(pwd)
 
-cd ../
-git clone https://github.com/felicityallen/SelfTarget.git
+cd tool/
+git clone https://github.com/felicityallen/SelfTarget
 cd SelfTarget
-# the python dependent
 pip install -r requirements.txt
-cd selftarget_pyutils
-pip install -e .
-cd ../indel_prediction
-pip install -e .
 
-cd ../indel_analysis/indelmap
+git sparse-checkout set --no-cone indel_analysis/indelmap
+git checkout
+
+# the python dependent
+# cd selftarget_pyutils
+# pip install -e .
+# cd ../indel_prediction
+# pip install -e .
+
+cd indel_analysis/indelmap
 cmake . -DINDELMAP_OUTPUT_DIR="$main_dir/tool"
 make && make install
 export INDELGENTARGET_EXE="$main_dir/tool"
@@ -18,3 +22,15 @@ export INDELGENTARGET_EXE="$main_dir/tool"
 cd $main_dir
 echo 'Indelgen="'$main_dir/tool/indelgentarget'"' >> inDecay/PATH.py
 echo 'Tooldir="'$main_dir/tool/indelgentarget'"' >> scripts/PATH.py
+
+if [ -e tool/indelgen ]
+then
+    rm -rf tool/SelfTarget
+    printf "\n=================\n"
+    echo "indelgen installed !"
+    printf "=================\n"
+else
+    echo "error auto-installing"
+    echo "please install indelgen mannually"
+    echo "via the instruction under https://github.com/felicityallen/SelfTarget/tree/master"
+fi
