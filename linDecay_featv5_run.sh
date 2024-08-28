4cc82d5b1ec6189ad3451e8171ff7a5cd00d6230
oZwZo <zhengwzh5@hotmail.com>
2024-08-28 19:52:08 +0800

    oligo list for fintuning

--- linDecay_featv5_run.sh
+++ linDecay_featv5_run.sh
@@ -5,11 +5,11 @@ e2=ST_June_2017_BOB_LV7A_DPI7
 
 run_ETG_BOB(){
     feat_i=$1
-    python scripts/STfeatv5_inDecay.py -E ${e1} -C 500 -G ${GPU1} -S "$feat_i:$((feat_i+30))" -M ST_Decay > ETG_training.log &
-    python scripts/STfeatv5_inDecay.py -E ${e1} -C 500 -G ${GPU1} -S "$((feat_i+30)):$((feat_i+60))" -M ST_Decay > ETG_training.log &
+    python scripts/STfeatv5_inDecay.py -E ${e1} -C 500 -G 0 -S "$feat_i:$((feat_i+30))" -M ST_Decay > ETG_training.log &
+    python scripts/STfeatv5_inDecay.py -E ${e1} -C 500 -G 1 -S "$((feat_i+30)):$((feat_i+60))" -M ST_Decay > ETG_training.log &
 
-    python scripts/STfeatv5_inDecay.py -E ${e2} -C 500 -G ${GPU2} -S "$feat_i:$((feat_i+30))" -M ST_Decay > ETG_training.log &
-    python scripts/STfeatv5_inDecay.py -E ${e2} -C 500 -G ${GPU2} -S "$((feat_i+30)):$((feat_i+60))" -M ST_Decay > ETG_training.log &
+    python scripts/STfeatv5_inDecay.py -E ${e2} -C 500 -G 2 -S "$feat_i:$((feat_i+30))" -M ST_Decay > ETG_training.log &
+    python scripts/STfeatv5_inDecay.py -E ${e2} -C 500 -G 3 -S "$((feat_i+30)):$((feat_i+60))" -M ST_Decay > ETG_training.log &
 }
 
 for i in {0..275..60};do
